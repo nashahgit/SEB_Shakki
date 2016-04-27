65095fb3622b2d833c2fb964048da3d43a12ff15
nashahgit <nashah25@gmail.com>
2016-04-27 20:33:29 +0530

    Changes as per the request

--- index.php
+++ index.php
@@ -23,6 +23,15 @@
 	   	<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
 		<script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/ScrollToPlugin.min.js"></script>
 	   	<script src="script.js"></script>
+	   	<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-76871462-1', 'auto');
+			ga('send', 'pageview');
+		</script>
 	    
 	</head>
   	<body>
@@ -218,7 +227,7 @@
 							  	</div>
 							  	<img class="img-responsive" src="operate_2.png" width="100%" alt="Card image">
 								<div class="card-block">
-								   <p class="card-text">We can take over struggling educational institutions. Make a detailed study and suggest improvement plans...</p>
+								   <p class="card-text">We help educational institutions in the Start Up mode. Make a detailed study and suggest improvement plans to increase...</p>
 								   <a href="services.html" class="btn btn-primary">Read More</a>
 								</div>
 							</div>
